a80cd6a8f052a143476e015eb02cbc1a12210dbc
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-07-13 18:47:47 +0500

    top-navigation done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        gray:'#F1ECFF'
+        
+        gray:{
+          1:'#F1ECFF',
+          2:'#F9F9FA'
+        },
+        hoverBg:'#F1ECFF',
+    
+        black:{
+          1:'#000000',
+          2:'#171717'
+        },
+        white:{
+          1:'#ffffff',
+          2:'#D4D4DA'
+        },
+        purple:{
+          1:'#7B53E7'
+        }
       }
     },
   },
